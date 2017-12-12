ef233d8a95e166bf428f2a0bc3dda5e6382e4f46
Luba Titova <calamondin89@gmail.com>
2017-12-12 18:36:33 +0200

    add map model

---
+++ src/main/java/org/nau/pacman/map/PacModel.java
@@ -0,0 +1,16 @@
+package org.nau.pacman.map;
+
+import org.newdawn.slick.Color;
+import org.nau.pacman.map.node.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class PacModel {
+    private Node[][] map;
+
+    public PacModel(Node[][] map) {
+
+    }
+}
